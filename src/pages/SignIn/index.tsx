6458b368e8dbb0b78131b4f75626186d332b558d
robertokbr <robertojuniordev@gmail.com>
2020-09-19 16:49:39 -0300

    Add SignIn Page

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -1,14 +1,58 @@
 import React from 'react';
-import { View, Image } from 'react-native';
-import { Container, Title } from './styles';
+import {
+  KeyboardAvoidingView,
+  Image,
+  Platform,
+  View,
+  ScrollView,
+} from 'react-native';
+import { Feather } from '@expo/vector-icons';
+import * as S from './styles';
 import logoImg from '../../assets/logo.png';
+import Button from '../../components/Button';
+import Input from '../../components/Input';
 
 const SignIn: React.FC = () => {
   return (
-    <Container>
-      <Image source={logoImg} />
-      <Title>Faça seu Logon</Title>
-    </Container>
+    <>
+      <KeyboardAvoidingView
+        behavior={Platform.OS === 'ios' ? 'padding' : undefined}
+        style={{ flex: 1 }}
+        enabled
+      >
+        <ScrollView
+          keyboardShouldPersistTaps="handled"
+          contentContainerStyle={{ flex: 1 }}
+        >
+          <S.Container>
+            <Image source={logoImg} />
+            <View>
+              <S.Title>Faça seu Logon</S.Title>
+            </View>
+            <Input icon="mail" name="email" placeholder="E-mail" />
+            <Input icon="lock" name="password" placeholder="Senha" />
+            <Button
+              onPress={() => {
+                console.log(true);
+              }}
+            >
+              Entrar
+            </Button>
+            <S.ForgotPassword
+              onPress={() => {
+                console.log(true);
+              }}
+            >
+              <S.ForgotPasswordText>Esquci minha senha</S.ForgotPasswordText>
+            </S.ForgotPassword>
+          </S.Container>
+        </ScrollView>
+      </KeyboardAvoidingView>
+      <S.CreateAccountButton>
+        <S.CreateAccountButtonText>Criar conta</S.CreateAccountButtonText>
+        <Feather name="log-in" size={20} color="#ff9000" />
+      </S.CreateAccountButton>
+    </>
   );
 };
 
--- src/pages/SignIn/styles.ts
+++ src/pages/SignIn/styles.ts
@@ -1,14 +1,46 @@
 import styled from 'styled-components/native';
+import { getBottomSpace } from 'react-native-iphone-x-helper';
 
 export const Container = styled.View`
   flex: 1;
   align-items: center;
   justify-content: center;
+  padding: 0 30px;
 `;
 
 export const Title = styled.Text`
   font-size: 24px;
   margin: 64px 0 24px;
   color: #f4ede8;
-  font-family: 'Roboto_500Medium';
+  font-family: medium;
+`;
+
+export const ForgotPassword = styled.TouchableOpacity`
+  margin-top: 24px;
+`;
+export const ForgotPasswordText = styled.Text`
+  color: #f4ede8;
+  font-size: 16px;
+  font-family: regular;
+`;
+
+export const CreateAccountButton = styled.TouchableOpacity`
+  flex-direction: row;
+  position: absolute;
+  left: 0;
+  bottom: 0;
+  right: 0;
+  background: #312e38;
+  border-top-width: 1px;
+  border-color: #232129;
+  padding: 16px 0 ${16 + getBottomSpace()}px;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const CreateAccountButtonText = styled.Text`
+  color: #ff9000;
+  font-size: 18px;
+  font-family: regular;
+  margin-left: 16px;
 `;
