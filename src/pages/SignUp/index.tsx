1d244236dfbe576d044e5d4057b5e87429705b53
robertokbr <robertojuniordev@gmail.com>
2020-09-29 22:08:14 -0300

    Add SignUp page

--- src/pages/SignUp/index.tsx
+++ src/pages/SignUp/index.tsx
@@ -1,8 +1,59 @@
-import React from 'react';
-import { View } from 'react-native';
+import React, { useCallback } from 'react';
+import {
+  KeyboardAvoidingView,
+  Image,
+  Platform,
+  View,
+  ScrollView,
+} from 'react-native';
+import { Feather } from '@expo/vector-icons';
+import { useNavigation } from '@react-navigation/native';
+import * as S from './styles';
+import logoImg from '../../assets/logo.png';
+import Button from '../../components/Button';
+import Input from '../../components/Input';
 
 const SignUp: React.FC = () => {
-  return <View />;
+  const navigation = useNavigation();
+  const handleNavigate = useCallback(() => {
+    navigation.goBack();
+  }, [navigation]);
+  return (
+    <>
+      <KeyboardAvoidingView
+        behavior={Platform.OS === 'ios' ? 'padding' : undefined}
+        style={{ flex: 1 }}
+        enabled
+      >
+        <ScrollView
+          keyboardShouldPersistTaps="handled"
+          contentContainerStyle={{ flex: 1 }}
+        >
+          <S.Container>
+            <Image source={logoImg} />
+            <View>
+              <S.Title>Crie sua conta</S.Title>
+            </View>
+            <Input icon="user" name="name" placeholder="Name" />
+            <Input icon="mail" name="email" placeholder="E-Mail" />
+            <Input icon="lock" name="password" placeholder="Senha" />
+
+            <Button
+              onPress={() => {
+                console.log(true);
+              }}
+            >
+              Entrar
+            </Button>
+          </S.Container>
+        </ScrollView>
+      </KeyboardAvoidingView>
+      <S.BackToSignIn onPress={handleNavigate}>
+        <Feather name="arrow-left" size={20} color="#fff" />
+        <S.BackToSignInText>Voltar para Logon</S.BackToSignInText>
+      </S.BackToSignIn>
+    </>
+  );
 };
 
 export default SignUp;
--- src/pages/SignUp/styles.ts
+++ src/pages/SignUp/styles.ts
@@ -1,3 +1,46 @@
 import styled from 'styled-components/native';
+import { getBottomSpace } from 'react-native-iphone-x-helper';
+import { Platform } from 'react-native';
 
-export const Container = styled.View``;
+export const Container = styled.View`
+  flex: 1;
+  align-items: center;
+  justify-content: center;
+  padding: 0 30px ${Platform.OS === 'android' ? '120px' : '60px'};
+`;
+
+export const Title = styled.Text`
+  font-size: 24px;
+  margin: 64px 0 24px;
+  color: #f4ede8;
+  font-family: medium;
+`;
+
+export const ForgotPassword = styled.TouchableOpacity`
+  margin-top: 24px;
+`;
+export const ForgotPasswordText = styled.Text`
+  color: #f4ede8;
+  font-size: 16px;
+  font-family: regular;
+`;
+
+export const BackToSignIn = styled.TouchableOpacity`
+  flex-direction: row;
+  position: absolute;
+  left: 0;
+  bottom: 0;
+  right: 0;
+  background: #312e38;
+  box-shadow: 1px 5px 10px rgba(0, 0, 0, 0.2);
+  padding: 16px 0 ${16 + getBottomSpace()}px;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const BackToSignInText = styled.Text`
+  color: #fff;
+  font-size: 18px;
+  font-family: regular;
+  margin-left: 16px;
+`;
