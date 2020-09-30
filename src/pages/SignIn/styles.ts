9f42bd57573298ea221d35de576591ac2e66c123
robertokbr <robertojuniordev@gmail.com>
2020-09-29 22:08:01 -0300

    Add padding dinamic by platform

--- src/pages/SignIn/styles.ts
+++ src/pages/SignIn/styles.ts
@@ -1,11 +1,12 @@
 import styled from 'styled-components/native';
 import { getBottomSpace } from 'react-native-iphone-x-helper';
+import { Platform } from 'react-native';
 
 export const Container = styled.View`
   flex: 1;
   align-items: center;
   justify-content: center;
-  padding: 0 30px;
+  padding: 0 30px ${Platform.OS === 'android' ? '180px' : '100px'};
 `;
 
 export const Title = styled.Text`
@@ -31,8 +32,7 @@ export const CreateAccountButton = styled.TouchableOpacity`
   bottom: 0;
   right: 0;
   background: #312e38;
-  border-top-width: 1px;
-  border-color: #232129;
+  box-shadow: 1px 5px 10px rgba(0, 0, 0, 0.2);
   padding: 16px 0 ${16 + getBottomSpace()}px;
   justify-content: center;
   align-items: center;
