fdd6c1040ef9c444ca628ddc70c24a4a8f69c069
robertokbr <robertojuniordev@gmail.com>
2020-09-29 23:56:08 -0300

    Alter styles

--- src/pages/SignIn/styles.ts
+++ src/pages/SignIn/styles.ts
@@ -6,7 +6,7 @@ export const Container = styled.View`
   flex: 1;
   align-items: center;
   justify-content: center;
-  padding: 0 30px ${Platform.OS === 'android' ? '180px' : '100px'};
+  padding: 0 30px ${Platform.OS === 'android' ? '120px' : '60px'};
 `;
 
 export const Title = styled.Text`
