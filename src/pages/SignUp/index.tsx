f87e8b96d900090256b693c97bfb19491671bcba
robertokbr <robertojuniordev@gmail.com>
2020-10-01 22:25:59 -0300

    Add axios post /users

--- src/pages/SignUp/index.tsx
+++ src/pages/SignUp/index.tsx
@@ -5,15 +5,19 @@ import {
   Platform,
   View,
   ScrollView,
+  Alert,
 } from 'react-native';
 import { Feather } from '@expo/vector-icons';
 import { useNavigation } from '@react-navigation/native';
 import { Form } from '@unform/mobile';
 import { FormHandles } from '@unform/core';
+import * as Yup from 'yup';
 import * as S from './styles';
 import logoImg from '../../assets/logo.png';
 import Button from '../../components/Button';
 import Input from '../../components/Input';
+import getValidationErrors from '../../utils/getValidationErrors';
+import api from '../../services/api';
 
 const SignUp: React.FC = () => {
   const navigation = useNavigation();
@@ -22,9 +26,45 @@ const SignUp: React.FC = () => {
   const nameInputref = useRef(null);
   const passwordInputRef = useRef(null);
 
-  const handleSubmit = useCallback((data: Object) => {
-    console.log(data);
-  }, []);
+  interface SingUpFormData {
+    name: string;
+    email: string;
+    pasword: string;
+  }
+
+  const handleSubmit = useCallback(
+    async (data: SingUpFormData) => {
+      try {
+        formRef.current.setErrors({});
+
+        const schema = Yup.object().shape({
+          name: Yup.string().required('Nome é obrigatório'),
+          email: Yup.string()
+            .required('E-mail obrigatório')
+            .email('Digite um e-mail válido'),
+          password: Yup.string().min(6, 'Pelo menos 6 digitos'),
+        });
+        await schema.validate(data, {
+          abortEarly: false,
+        });
+
+        await api.post('/users', data);
+
+        navigation.goBack();
+      } catch (err) {
+        if (err instanceof Yup.ValidationError) {
+          const errors = getValidationErrors(err);
+          formRef.current.setErrors(errors);
+          return;
+        }
+        Alert.alert(
+          'Erro na autenticação',
+          'Ocorreu um erro ao fazer login, cheque as credenciais.',
+        );
+      }
+    },
+    [navigation],
+  );
 
   const handleNavigate = useCallback(() => {
     navigation.goBack();
