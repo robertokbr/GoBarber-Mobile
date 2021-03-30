b18189e3bb69846dc036d6469ca8eeb8febe60ff
robertokbr <robertojuniordev@gmail.com>
2021-03-30 20:44:38 -0300

    Update CustomAvatar component

--- src/components/CustomAvatar/index.tsx
+++ src/components/CustomAvatar/index.tsx
@@ -3,12 +3,17 @@ import * as S from './styles';
 
 interface CustomAvatarProps {
   avatar: string;
+  imageStyle?: Object;
 }
 
-const CustomAvatar: React.FC<CustomAvatarProps> = ({ children, avatar }) => {
+const CustomAvatar: React.FC<CustomAvatarProps> = ({
+  children,
+  avatar,
+  imageStyle,
+}) => {
   return (
     <S.Container>
-      <S.Avatar source={{ uri: avatar }} />
+      <S.Avatar style={imageStyle} source={{ uri: avatar }} />
       {children}
     </S.Container>
   );
