9509ca7918d916e493d3b44d103201516bc10832
robertokbr <robertojuniordev@gmail.com>
2021-03-30 20:44:58 -0300

    Initi CreateAppointment providers list

--- src/pages/CreateAppointment/index.tsx
+++ src/pages/CreateAppointment/index.tsx
@@ -1,20 +1,49 @@
-import React, { ReactNode } from 'react';
-
+import React, { useEffect, useState } from 'react';
+import { useRoute } from '@react-navigation/native';
 import { Text } from 'react-native';
 
-import { Container } from './styles';
+import * as S from './styles';
+import Header from '../../components/Header';
+import CustomAvatar from '../../components/CustomAvatar';
+import { useAuth } from '../../hooks/auth';
+import api from '../../services/api';
+import IUser from '../../models/IUser';
 
-interface CreateAppointmentProps {
-  children: ReactNode;
+interface CreateAppointmentParams {
+  params: {
+    provider_id: string;
+  };
 }
 
-function CreateAppointment({ children }: CreateAppointmentProps) {
+const CreateAppointment: React.FC = ({ children }) => {
+  const { params } = useRoute() as CreateAppointmentParams;
+  const { user } = useAuth();
+
+  const [providers, setProviders] = useState<IUser[]>([]);
+
+  useEffect(() => {
+    api
+      .get<IUser[]>('/providers')
+      .then(response => setProviders(response.data));
+  }, [providers]);
+
   return (
-    <Container>
-      <Text>CreateAppointment</Text>
-      {children}
-    </Container>
+    <S.Container>
+      <Header subTitle="Cabeleireiros" withBackIcon>
+        <CustomAvatar avatar={user.avatar_url} />
+      </Header>
+      <S.ProvidersList
+        horizontal
+        data={providers}
+        keyExtractor={provider => provider.id}
+        renderItem={({ item: provider, index }) => (
+          <S.ProviderContainer isTheLast={index + 1 === providers.length}>
+            <S.ProviderName>{provider.name}</S.ProviderName>
+          </S.ProviderContainer>
+        )}
+      />
+    </S.Container>
   );
-}
+};
 
 export default CreateAppointment;
--- src/pages/CreateAppointment/styles.ts
+++ src/pages/CreateAppointment/styles.ts
@@ -1,5 +1,30 @@
+import { FlatList, RectButton } from 'react-native-gesture-handler';
 import styled from 'styled-components/native';
+import IUser from '../../models/IUser';
+
+interface ListProps {
+  isTheLast: boolean;
+}
 
 export const Container = styled.View`
+  flex: 1;
+`;
 
+export const ProvidersList = styled(FlatList as new () => FlatList<IUser>)`
+  padding: 32px 24px 16px;
+`;
+
+export const ProviderContainer = styled(RectButton)<ListProps>`
+  background: #3e3b47;
+  border-radius: 10px;
+  padding: 8px;
+  margin-right: ${props => (props.isTheLast ? 20 : 8)}px;
+  align-items: center;
+  height: 112px;
+`;
+
+export const ProviderName = styled.Text`
+  font-family: regular;
+  font-size: 12px;
+  color: #f4ede8;
 `;
