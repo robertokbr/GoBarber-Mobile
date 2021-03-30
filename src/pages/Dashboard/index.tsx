dfa78882b482cea4f8abb40524608a5380f978ba
robertokbr <robertojuniordev@gmail.com>
2021-03-30 20:43:55 -0300

    Finish Dashboard

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -2,6 +2,7 @@ import React, { useCallback, useEffect, useState } from 'react';
 import { Text } from 'react-native';
 import { RectButton } from 'react-native-gesture-handler';
 import { useNavigation } from '@react-navigation/native';
+import { Feather } from '@expo/vector-icons';
 import { useAuth } from '../../hooks/auth';
 import * as S from './styles';
 
@@ -26,6 +27,13 @@ const Dashboard: React.FC = () => {
     navigation.navigate('Profile');
   }, [navigation]);
 
+  const navigateToCreateAppointment = useCallback(
+    (provider_id: string) => {
+      navigation.navigate('CreateAppointment', { provider_id });
+    },
+    [navigation],
+  );
+
   return (
     <S.Container>
       <Header title="Bem vindo!" subTitle={user.name}>
@@ -33,16 +41,38 @@ const Dashboard: React.FC = () => {
           <CustomAvatar avatar={user.avatar_url} />
         </S.ProfileButton>
       </Header>
-      <RectButton
-        style={{ alignSelf: 'center', justifyContent: 'center' }}
-        onPress={() => signOut()}
-      >
-        <Text>Sair</Text>
-      </RectButton>
       <S.ProvidersList
         data={providers}
         keyExtractor={provider => provider.id}
-        renderItem={({ item }) => <Text>{item.name}</Text>}
+        ListHeaderComponent={
+          <S.ProviderListTitle>Cabeleireiros</S.ProviderListTitle>
+        }
+        renderItem={({ item: provider, index }) => (
+          <S.ProviderContainer
+            onPress={() => navigateToCreateAppointment(provider.id)}
+            isTheLast={index + 1 === providers.length}
+          >
+            <CustomAvatar
+              imageStyle={{
+                width: 72,
+                height: 72,
+                borderRadius: 36,
+              }}
+              avatar={provider.avatar_url}
+            />
+            <S.ProviderInfo>
+              <S.ProviderName>{provider.name}</S.ProviderName>
+              <S.ProviderMeta>
+                <Feather name="calendar" size={14} color="#ff9000" />
+                <S.ProviderMetaText>Segunda a Sexta</S.ProviderMetaText>
+              </S.ProviderMeta>
+              <S.ProviderMeta>
+                <Feather name="clock" size={14} color="#ff9000" />
+                <S.ProviderMetaText>08:00 ás 17:00</S.ProviderMetaText>
+              </S.ProviderMeta>
+            </S.ProviderInfo>
+          </S.ProviderContainer>
+        )}
       />
     </S.Container>
   );
--- src/pages/Dashboard/styles.ts
+++ src/pages/Dashboard/styles.ts
@@ -3,10 +3,55 @@ import { RectButton } from 'react-native-gesture-handler';
 import styled from 'styled-components/native';
 import IUser from '../../models/IUser';
 
+interface ListProps {
+  isTheLast: boolean;
+}
+
 export const Container = styled.View`
   flex: 1;
 `;
 
 export const ProfileButton = styled(RectButton)``;
 
-export const ProvidersList = styled(FlatList as new () => FlatList<IUser>)``;
+export const ProvidersList = styled(FlatList as new () => FlatList<IUser>)`
+  padding: 32px 24px 0;
+`;
+
+export const ProviderListTitle = styled.Text`
+  font-size: 24px;
+  margin-bottom: 24px;
+  color: #f4ede8;
+  font-family: medium;
+`;
+
+export const ProviderContainer = styled(RectButton)<ListProps>`
+  background: #3e3b47;
+  border-radius: 10px;
+  padding: 20px;
+  margin-bottom: ${props => (props.isTheLast ? 48 : 16)}px;
+  flex-direction: row;
+  align-items: center;
+`;
+
+export const ProviderInfo = styled.View`
+  flex: 1;
+  margin-left: 20px;
+`;
+
+export const ProviderName = styled.Text`
+  font-family: regular;
+  font-size: 18px;
+  color: #f4ede8;
+`;
+
+export const ProviderMeta = styled.View`
+  flex-direction: row;
+  align-items: center;
+  margin-top: 8px;
+`;
+
+export const ProviderMetaText = styled.Text`
+  margin-left: 8px;
+  color: #999591;
+  font-family: regular;
+`;
