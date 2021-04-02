4ba7df4b0ea1cb740dcce2e75218ad2dfe55535a
robertokbr <robertojuniordev@gmail.com>
2021-04-01 22:40:22 -0300

    Update CreateAppointment page

--- src/pages/CreateAppointment/index.tsx
+++ src/pages/CreateAppointment/index.tsx
@@ -1,6 +1,6 @@
-import React, { useEffect, useState } from 'react';
+import React, { useCallback, useEffect, useState } from 'react';
 import { useRoute } from '@react-navigation/native';
-import { Text } from 'react-native';
+import DatePicker from 'react-native-date-picker';
 
 import * as S from './styles';
 import Header from '../../components/Header';
@@ -15,11 +15,16 @@ interface CreateAppointmentParams {
   };
 }
 
-const CreateAppointment: React.FC = ({ children }) => {
+const CreateAppointment: React.FC = () => {
   const { params } = useRoute() as CreateAppointmentParams;
   const { user } = useAuth();
 
   const [providers, setProviders] = useState<IUser[]>([]);
+  const [selectedProvider, setSelectedProvider] = useState(params.provider_id);
+  const [showDatePicker, setShowDatePicker] = useState(false);
+  const [selectedDate, setSelectedDate] = useState(new Date());
+
+  const [providerDayAvailability, setProviderDayAvailability] = useState([]);
 
   useEffect(() => {
     api
@@ -27,21 +32,85 @@ const CreateAppointment: React.FC = ({ children }) => {
       .then(response => setProviders(response.data));
   }, [providers]);
 
+  useEffect(() => {
+    api
+      .get<any[]>(`/providers/${selectedProvider}/day-availability`, {
+        params: {
+          year: selectedDate.getFullYear(),
+          month: selectedDate.getMonth() + 1,
+          day: selectedDate.getDate(),
+        },
+      })
+      .then(response => {
+        console.log(response.data);
+        setProviderDayAvailability(response.data);
+      });
+  }, [selectedDate, selectedProvider]);
+
+  const handleSelectProvider = useCallback((provider_id: string) => {
+    setSelectedProvider(provider_id);
+  }, []);
+
+  const handleToggleDatePicker = useCallback(() => {
+    setShowDatePicker(state => !state);
+  }, []);
+
+  const handleDateChange = useCallback(
+    (date: Date) => {
+      const currentDate = date || selectedDate;
+
+      setSelectedDate(currentDate);
+    },
+    [selectedDate],
+  );
+
   return (
     <S.Container>
       <Header subTitle="Cabeleireiros" withBackIcon>
         <CustomAvatar avatar={user.avatar_url} />
       </Header>
-      <S.ProvidersList
-        horizontal
-        data={providers}
-        keyExtractor={provider => provider.id}
-        renderItem={({ item: provider, index }) => (
-          <S.ProviderContainer isTheLast={index + 1 === providers.length}>
-            <S.ProviderName>{provider.name}</S.ProviderName>
-          </S.ProviderContainer>
+      <S.ProviderListContainer>
+        <S.ProvidersList
+          showsHorizontalScrollIndicator={false}
+          horizontal
+          data={providers}
+          keyExtractor={provider => provider.id}
+          renderItem={({ item: provider, index }) => (
+            <S.ProviderContainer
+              isSelected={provider.id === selectedProvider}
+              isTheLast={index + 1 === providers.length}
+              onPress={() => handleSelectProvider(provider.id)}
+            >
+              <CustomAvatar
+                avatar={provider.avatar_url}
+                imageStyle={{
+                  height: 32,
+                  width: 32,
+                  borderRadius: 16,
+                }}
+              />
+              <S.ProviderName isSelected={provider.id === selectedProvider}>
+                {provider.name}
+              </S.ProviderName>
+            </S.ProviderContainer>
+          )}
+        />
+      </S.ProviderListContainer>
+      <S.CalendarContainer>
+        <S.CalendarTitle>Selecione a data</S.CalendarTitle>
+        <S.OpenDatePickerButton onPress={handleToggleDatePicker}>
+          <S.OpenDatePickerText>Selecionar data</S.OpenDatePickerText>
+        </S.OpenDatePickerButton>
+        {showDatePicker && (
+          <DatePicker
+            locale="pt"
+            mode="date"
+            androidVariant="iosClone"
+            date={selectedDate}
+            onDateChange={handleDateChange}
+          />
         )}
-      />
+      </S.CalendarContainer>
     </S.Container>
   );
 };
--- src/pages/CreateAppointment/styles.ts
+++ src/pages/CreateAppointment/styles.ts
@@ -4,27 +4,58 @@ import IUser from '../../models/IUser';
 
 interface ListProps {
   isTheLast: boolean;
+  isSelected: boolean;
 }
 
 export const Container = styled.View`
   flex: 1;
 `;
 
-export const ProvidersList = styled(FlatList as new () => FlatList<IUser>)`
-  padding: 32px 24px 16px;
-`;
-
-export const ProviderContainer = styled(RectButton)<ListProps>`
-  background: #3e3b47;
-  border-radius: 10px;
-  padding: 8px;
-  margin-right: ${props => (props.isTheLast ? 20 : 8)}px;
-  align-items: center;
+export const ProviderListContainer = styled.View`
   height: 112px;
 `;
 
-export const ProviderName = styled.Text`
-  font-family: regular;
-  font-size: 12px;
-  color: #f4ede8;
+export const ProvidersList = styled(FlatList as new () => FlatList<IUser>)`
+  padding: 32px 24px;
+`;
+
+export const ProviderContainer = styled(RectButton)<ListProps>`
+  background: ${props => (props.isSelected ? '#ff9000' : '#3e3b47')};
+  flex-direction: row;
+  padding: 8px 12px;
+  margin-right: ${props => (props.isTheLast ? 32 : 16)}px;
+  align-items: center;
+  justify-content: center;
+  border-radius: 10px;
+`;
+
+export const ProviderName = styled.Text<Omit<ListProps, 'isTheLast'>>`
+  font-family: medium;
+  font-size: 16px;
+  color: ${props => (props.isSelected ? '#232129' : '#f4ede8')};
+  margin-left: 8px;
+`;
+
+export const CalendarContainer = styled.View``;
+
+export const CalendarTitle = styled.Text`
+  font-family: medium;
+  font-size: 24px;
+  margin: 0 24px 24px;
+  color: #fff;
+`;
+
+export const OpenDatePickerButton = styled(RectButton)`
+  height: 46px;
+  background: #ff9000;
+  border-radius: 10px;
+  align-items: center;
+  justify-content: center;
+  margin: 0 24px;
+`;
+
+export const OpenDatePickerText = styled.Text`
+  font-family: medium;
+  font-size: 16px;
+  color: #232129;
 `;
