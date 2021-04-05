fd2bdab96bacac69504b39be222369e2f0091bda
robertokbr <robertojuniordev@gmail.com>
2021-04-05 20:23:24 -0300

    Add Appointments page

--- src/pages/AppointmentCreated/index.tsx
+++ src/pages/AppointmentCreated/index.tsx
@@ -1,20 +1,57 @@
-import React, { ReactNode } from 'react';
+import React, { useCallback, useMemo } from 'react';
+import { useNavigation, useRoute } from '@react-navigation/native';
+import { Feather } from '@expo/vector-icons';
+import { format } from 'date-fns';
+import ptBR from 'date-fns/locale/pt-BR';
 
-import { Text } from 'react-native';
+import * as S from './styles';
 
-import { Container } from './styles';
-
-interface AppointmentCreatedProps {
-  children: ReactNode;
+interface RouteParams {
+  params: {
+    date: number;
+  };
 }
 
-function AppointmentCreated({ children }: AppointmentCreatedProps) {
-  return (
-    <Container>
-      <Text>AppointmentCreated</Text>
-      {children}
-    </Container>
+const AppointmentCreated: React.FC = () => {
+  const {
+    params: { date },
+  } = useRoute() as RouteParams;
+
+  const { reset } = useNavigation();
+
+  const handleConfirm = useCallback(() => {
+    reset({
+      routes: [{ name: 'Dashboard' }],
+      index: 0,
+    });
+  }, [reset]);
+
+  const formattedDate = useMemo(
+    () =>
+      format(
+        new Date(date),
+        "EEEE', dia' dd 'de' MMMM 'de' yyyy 'às' HH:mm'h'",
+        {
+          locale: ptBR,
+        },
+      ),
+    [date],
   );
-}
+
+  return (
+    <S.Container>
+      <Feather name="check" size={80} color="#04d361" />
+      <S.MessageTitle>
+        Agendamento
+        {'\n'}
+        concluído
+      </S.MessageTitle>
+      <S.MessageDetails>{formattedDate}</S.MessageDetails>
+      <S.ConfirmationButton onPress={handleConfirm}>
+        <S.ConfirmationButtonText>Ok</S.ConfirmationButtonText>
+      </S.ConfirmationButton>
+    </S.Container>
+  );
+};
 
 export default AppointmentCreated;
--- src/pages/AppointmentCreated/styles.ts
+++ src/pages/AppointmentCreated/styles.ts
@@ -1,5 +1,40 @@
+import { RectButton } from 'react-native-gesture-handler';
 import styled from 'styled-components/native';
 
 export const Container = styled.View`
+  flex: 1;
+  justify-content: center;
+  align-items: center;
+  padding: 0 46px;
+`;
 
+export const MessageTitle = styled.Text`
+  font-size: 32px;
+  color: #f4ede8;
+  font-family: medium;
+  margin-top: 48px;
+  text-align: center;
+`;
+
+export const MessageDetails = styled.Text`
+  font-family: regular;
+  font-size: 16px;
+  color: #999591;
+  margin-top: 16px;
+`;
+
+export const ConfirmationButton = styled(RectButton)`
+  background: #ff9000;
+  width: 100%;
+  padding: 12px 12px;
+  align-items: center;
+  justify-content: center;
+  border-radius: 10px;
+  margin-top: 24px;
+`;
+
+export const ConfirmationButtonText = styled.Text`
+  font-family: medium;
+  color: #fff;
+  font-size: 18px;
 `;
