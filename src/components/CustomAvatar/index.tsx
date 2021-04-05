d889b859185b35e6b504f485a1c7b1190024b9d4
robertokbr <robertojuniordev@gmail.com>
2021-04-05 20:22:24 -0300

    Update custom avatar to handle users without image

--- src/components/CustomAvatar/index.tsx
+++ src/components/CustomAvatar/index.tsx
@@ -1,20 +1,31 @@
-import React from 'react';
+import React, { useMemo } from 'react';
 import * as S from './styles';
 
 interface CustomAvatarProps {
   avatar: string;
+  userName: string;
   imageStyle?: Object;
 }
 
 const CustomAvatar: React.FC<CustomAvatarProps> = ({
-  children,
   avatar,
   imageStyle,
+  userName,
 }) => {
+  const formattedUserName = useMemo(
+    () => userName.trim().split('')[0].toUpperCase(),
+    [userName],
+  );
+
   return (
     <S.Container>
-      <S.Avatar style={imageStyle} source={{ uri: avatar }} />
-      {children}
+      {avatar ? (
+        <S.Avatar style={imageStyle} source={{ uri: avatar }} />
+      ) : (
+        <S.UserNameContainer style={imageStyle}>
+          <S.UserName>{formattedUserName}</S.UserName>
+        </S.UserNameContainer>
+      )}
     </S.Container>
   );
 };
--- src/components/CustomAvatar/styles.ts
+++ src/components/CustomAvatar/styles.ts
@@ -7,3 +7,18 @@ export const Avatar = styled.Image`
   width: 56px;
   border-radius: 28px;
 `;
+
+export const UserNameContainer = styled.View`
+  background: #232129;
+  height: 56px;
+  width: 56px;
+  border-radius: 28px;
+  align-items: center;
+  justify-content: center;
+`;
+
+export const UserName = styled.Text`
+  font-family: regular;
+  font-size: 24px;
+  color: #fff;
+`;
