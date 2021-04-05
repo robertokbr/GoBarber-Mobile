c5068b4e44154fde13444608d6b1812660e9e956
robertokbr <robertojuniordev@gmail.com>
2021-04-05 20:22:33 -0300

    Update appointments return

--- src/models/IAppointments.ts
+++ src/models/IAppointments.ts
@@ -4,10 +4,16 @@ export default interface IAppointments {
   id: string;
   provider_id: string;
   user_id: string;
-  formatedHour: string;
+  formattedHour: string;
+  formattedDate: string;
   date: string;
   created_at: string;
   updated_at: string;
   user_image: string;
   user: IUser;
+  provider: {
+    userAccount: {
+      user: IUser;
+    };
+  };
 }
